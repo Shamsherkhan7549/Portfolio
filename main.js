5b1ec8a1777bc654a819ab6ce397fe76bf41cdb2
Shamsher Khan <shamsherkhan7549@gmail.com>
2024-10-07 00:59:21 +0530

    Portfolio

---
+++ main.js
@@ -0,0 +1,51 @@
+
+let menuIcon = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+
+
+
+// menuIcon.addEventListener("click", () => {
+//     if(navbar.style.right === '0px'){
+//         navbar.style.right= '-160px';
+//     }else{
+//         navbar.style.right ='0px'
+//     }
+// })
+
+let icon1 = document.querySelector(".navbar :nth-child(1)");
+let icon2 = document.querySelector(".navbar :nth-child(2)");
+let icon3 = document.querySelector(".navbar :nth-child(3)");
+let icon4 = document.querySelector(".navbar :nth-child(4)");
+
+menuIcon.addEventListener('click', () => {
+    if(icon1.style.right === '2rem'){
+        icon1.style.right = '-150px'
+    }else{
+        icon1.style.right = '2rem'
+    }
+
+    if(icon2.style.right === '2rem'){
+        icon2.style.right = '-300px'
+    }else{
+        icon2.style.right = '2rem'
+    }
+
+    if(icon3.style.right === '2rem'){
+        icon3.style.right = '-450px'
+    }else{
+        icon3.style.right = '2rem'
+    }
+
+    if(icon4.style.right === '2rem'){
+        icon4.style.right = '-600px'
+    }else{
+        icon4.style.right = '2rem'
+    }
+});
+
+let readBtn = document.querySelector('#read-btn');
+let readMore = document.querySelector('.read-more')
+
+readBtn.addEventListener('click', () => {
+    readMore.style.display = "block"
+})
